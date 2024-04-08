0078e191046088c0955e06e40185d16500149d0a
littleTurnip <lib-dev@outlook.com>
2024-04-08 23:26:32 +0800

    feat: Add ContentView and DemoApp for initial setup.

---
+++ Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by littleTurnip on 4/8/24.
+//
+
+import DMLPlayer
+import DMLPlayerProtocol
+import SwiftUI
+
+// MARK: - ContentView
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello world")
+  }
+}
+
+#Preview {
+  ContentView()
+}
