ccab1df7abda866b8f1fbaa4f2e1247f4fbf23de
littleTurnip <lib-dev@outlook.com>
2024-04-09 23:59:20 +0800

    refactor: Update PlayInfo and PlayableItem protocols

--- Sources/DMLPlayerProtocol/PlayerResource.swift
+++ Sources/DMLPlayerProtocol/PlayerResource.swift
@@ -4,53 +4,108 @@
 //
 //  Created by littleTurnip on 3/1/24.
 //
+import OSLog
 import SwiftUI
 
-// MARK: - PlayerResource
+// MARK: - PlayInfo
 
-public protocol PlayerResource: Sendable {
-  var line: String { get }
-  var rate: Int { get }
-  var url: URL? { get }
-  var rateList: [any StreamRate] { get }
-  var cdnList: [any StreamCDN] { get }
-  var cdnName: String { get }
-  var resolution: String { get }
-}
-
-// MARK: - StreamRate
-
-public protocol StreamRate: Identifiable, Sendable {
-  var id: Int { get }
-  var resolution: String { get }
-}
-
-// MARK: - StreamCDN
-
-public protocol StreamCDN: Identifiable, Sendable {
-  var id: String { get }
-  var cdnName: String { get }
+public struct PlayInfo: Sendable {
+  public var isFav: Bool = false
+  public var cdnLine: String?
+  public var playCount: Int = 0
+  public var lastPlay: Date = .init()
+  public init(isFav: Bool = false, playCount: Int = 0, cdnLine: String? = nil, lastPlay: Date = Date()) {
+    self.isFav = isFav
+    self.playCount = playCount
+    self.cdnLine = cdnLine
+    self.lastPlay = lastPlay
+  }
 }
 
 // MARK: - PlayableItem
 
-public protocol PlayableItem {
+public protocol PlayableItem: ObservableObject, Identifiable {
+  associatedtype LiveInfo
   var id: String { get }
   var platform: String { get }
   var roomID: String { get }
   var helperID: String? { get }
+  var liveInfo: LiveInfo { get set }
 
   // MARK: - local properties
 
-  var isFav: Bool { get set }
-  var savedCDN: String? { get set }
-  var playCount: Int { get set }
-  var lastPlayTime: Date { get set }
+//  var isFav: Bool { get set }
+//  var savedCDN: String? { get set }
+//  var playCount: Int { get set }
+//  var lastPlayTime: Date { get set }
+
+  var playerInfo: PlayInfo { get set }
+  var currentResource: LiveResource? { get set }
+  var danmakuService: DanmakuService? { get }
+  /// An `AsyncStream` of `PlayerResource` objects.
+  var resourceStream: AsyncStream<LiveResource?> { get }
+  var resourceContinuation: AsyncStream<LiveResource?>.Continuation? { get set }
+  init(playerArgs: PlayInfo?, with liveInfo: LiveInfo)
+
   func update()
 
-  // MARK: - stream resource
+  /// method to update livestream resource
+  /// - Parameter newResource: `PlayerResource` object
+  func updateResource(with newResource: LiveResource?)
+  /// fetch livestream resource
+  /// - Parameters:
+  ///  - line: Name string representing the CDN line
+  ///  - rate: Rate value for the stream
+  func loadResource(line: String?, rate: Int?)
+  func fetchResource(line: String?, rate: Int?) async -> LiveResource?
+}
 
-  var currentResource: PlayerResource? { get set }
-  var resourceStream: AsyncStream<PlayerResource?> { get }
-  func loadStream(line: String?, rate: Int?)
+public extension PlayableItem {
+  var logger: Logger { Logger(subsystem: "DMLPlayer", category: "PlayableItem") }
+}
+
+/// add stream resource related methods
+public extension PlayableItem {
+  var resourceStream: AsyncStream<LiveResource?> {
+    AsyncStream { [weak self] continuation in
+      self?.resourceContinuation = continuation
+    }
+  }
+
+  func updateResource(with newResource: LiveResource?) {
+    logger.trace("updateResource")
+    resourceContinuation?.yield(newResource)
+  }
+
+  func loadResource(line: String? = nil, rate: Int? = nil) {
+    logger.trace("loadStream")
+    Task { @MainActor in
+      let streamResource = await self.fetchResource(line: line, rate: rate)
+      #if DEBUG
+        debugPrint(streamResource.debugDescription)
+      #endif
+      await MainActor.run {
+        currentResource = streamResource
+        self.updateResource(with: streamResource)
+      }
+    }
+  }
+}
+
+public extension PlayableItem {
+  func plusPlayCount() {
+    playerInfo.playCount += 1
+  }
+
+  func setCDNLine() {
+    playerInfo.cdnLine = currentResource?.line
+  }
+
+  func setLastPlayTime() {
+    playerInfo.lastPlay = Date()
+  }
+
+  func toggleFav() {
+    playerInfo.isFav.toggle()
+  }
 }
