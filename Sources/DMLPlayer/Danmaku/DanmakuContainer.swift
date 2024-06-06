8b78eee24aad757fba10e21c4a5a043d661052f5
littleTurnip <lib-dev@outlook.com>
2024-06-06 15:50:47 +0800

    feat: Implement danmaku blocking feature

--- Sources/DMLPlayer/Danmaku/DanmakuContainer.swift
+++ Sources/DMLPlayer/Danmaku/DanmakuContainer.swift
@@ -50,6 +50,7 @@ extension DanmakuContainer {
   public class Coordinator: DanmakuDelegate, ObservableObject, @unchecked Sendable {
     private let logger = Logger(subsystem: "DMLPlayer", category: "Danmaku.Coordinator")
     var danmakuService: DanmakuService?
+    var blockKeywords: Set<String> = []
     var uiView: DanmakuView?
 
     init(service: DanmakuService? = nil) {
@@ -74,7 +75,7 @@ extension DanmakuContainer {
       logger.debug("start danmaku stream")
       Task { @MainActor in
         await danmakuService?.setDanmakuHandler { [weak self] danmaku in
-          self?.shootDanmaku(danmaku, fontSize: options.danmakuFontSize, speed: options.danmakuSpeed)
+          await self?.shootDanmaku(danmaku, fontSize: options.danmakuFontSize, speed: options.danmakuSpeed)
         }
         await danmakuService?.start()
       }
@@ -89,11 +90,25 @@ extension DanmakuContainer {
       }
     }
 
-    @Sendable func shootDanmaku(_ danmaku: Danmaku, fontSize: CGFloat, speed: Double) {
-      Task { @MainActor in
+    @MainActor
+    func shootDanmaku(_ danmaku: Danmaku, fontSize: CGFloat, speed: Double) {
+      // 判断是否包含屏蔽词
+      if isDanmakuInSet(danmaku, in: blockKeywords) {
+        return
+      } else {
         let model = TextDanmakuModel(danmaku, fontSize: fontSize, speed: speed)
         uiView?.shoot(danmaku: model)
       }
+    }
+
+    private func isDanmakuInSet(_ danmaku: Danmaku, in keywordSet: Set<String>) -> Bool {
+      guard !keywordSet.isEmpty else { return false }
+      let pattern = keywordSet.map { NSRegularExpression.escapedPattern(for: $0) }.joined(separator: "|")
+      guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+        return false
+      }
+      let matches = regex.firstMatch(in: danmaku.text, options: [], range: NSRange(location: 0, length: danmaku.text.utf16.count))
+      return matches != nil
     }
   }
 }
