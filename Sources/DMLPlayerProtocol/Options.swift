557e6e1eeeec990c29c91abd0c66344c9b26ae6b
littleTurnip <lib-dev@outlook.com>
2024-08-07 17:12:45 +0800

    refactor: Refactor DanmakuOptions structure with separate Layer and Danmaku models. refactor: Update default DanmakuOptions property names.

--- Sources/DMLPlayerProtocol/Options.swift
+++ Sources/DMLPlayerProtocol/Options.swift
@@ -32,29 +32,87 @@ public class PlayerOptions: KSOptions {
 // MARK: - DanmakuOptions
 
 public struct DanmakuOptions {
-  public var danmakuViewHeight: CGFloat
-  public var danmakuOpacity: Double
-  public var danmakuTrackHeight: CGFloat
-  public var danmakuFontSize: CGFloat
-  public var danmakuSpeed: Double
-  public var isDanmakuAutoPlay: Bool
-  public var blockKeywords: Set<String> = []
+  public var danmaku: Self.Danmaku
+  public var layer: Self.Layer
+  public var blockKeywords: Set<String>
 
   public init(
-    danmakuViewHeight: CGFloat = 550,
-    danmakuOpacity: Double = 0.8,
-    danmakuTrackHeight: CGFloat = 1.4,
-    danmakuFontSize: CGFloat = 34,
-    danmakuSpeed: Double = 10,
-    isDanmakuAutoPlay: Bool = true,
-    blockKeywords: Set<String> = []
+    viewHeight: CGFloat,
+    opacity: Double,
+    trackHeight: CGFloat,
+    fontSize: CGFloat,
+    speed: Double,
+    isAutoPlay: Bool,
+    isColor: Bool,
+    blockKeywords: Set<String>
   ) {
-    self.danmakuViewHeight = danmakuViewHeight
-    self.danmakuOpacity = danmakuOpacity
-    self.danmakuTrackHeight = danmakuTrackHeight
-    self.danmakuFontSize = danmakuFontSize
-    self.danmakuSpeed = danmakuSpeed
-    self.isDanmakuAutoPlay = isDanmakuAutoPlay
+    danmaku = Self.Danmaku(
+      speed: speed,
+      isColor: isColor,
+      fontSize: fontSize
+    )
+    layer = Self.Layer(
+      viewHeight: viewHeight,
+      opacity: opacity,
+      trackHeight: trackHeight,
+      isAutoPlay: isAutoPlay
+    )
     self.blockKeywords = blockKeywords
   }
+
+  public init(layer: Self.Layer, danmaku: Self.Danmaku, blockKeywords: Set<String>) {
+    self.layer = layer
+    self.danmaku = danmaku
+    self.blockKeywords = blockKeywords
+  }
+}
+
+public extension DanmakuOptions {
+  struct Layer {
+    public var viewHeight: CGFloat
+    public var opacity: Double
+    public var trackHeight: CGFloat
+    public var isAutoPlay: Bool
+
+    public init(
+      viewHeight: CGFloat,
+      opacity: Double,
+      trackHeight: CGFloat,
+      isAutoPlay: Bool
+    ) {
+      self.viewHeight = viewHeight
+      self.opacity = opacity
+      self.trackHeight = trackHeight
+      self.isAutoPlay = isAutoPlay
+    }
+  }
+
+  struct Danmaku {
+    public var speed: Double
+    public var isColor: Bool
+    public var fontSize: CGFloat
+
+    public init(
+      speed: Double,
+      isColor: Bool,
+      fontSize: CGFloat
+    ) {
+      self.speed = speed
+      self.isColor = isColor
+      self.fontSize = fontSize
+    }
+  }
+}
+
+public extension DanmakuOptions {
+  static var `default`: Self = .init(
+    viewHeight: 550,
+    opacity: 0.8,
+    trackHeight: 1.4,
+    fontSize: 34,
+    speed: 10,
+    isAutoPlay: true,
+    isColor: true,
+    blockKeywords: []
+  )
 }
