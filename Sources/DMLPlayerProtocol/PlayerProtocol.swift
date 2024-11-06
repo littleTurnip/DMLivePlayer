4cbf78745a0c1fdbe33ebd0d7c26f6911f190a13
littleTurnip <lib-dev@outlook.com>
2024-11-06 17:28:14 +0800

    refactor: remove unused properties from PlayerProtocol

--- Sources/DMLPlayerProtocol/PlayerProtocol.swift
+++ Sources/DMLPlayerProtocol/PlayerProtocol.swift
@@ -17,9 +17,7 @@ public typealias PlayerCoordinator = KSVideoPlayer.Coordinator
 public protocol PlayerProtocol: AnyObject {
   var item: (any PlayableItem)? { get set }
   var playlists: [any Playlist] { get set }
-  var playlistMap: [UUID: Set<String>] { get }
-  var playerCoordinator: PlayerCoordinator { get }
-  var danmakuCoordinator: DanmakuCoordinator { get }
+
   var playerOptions: PlayerOptions { get set }
   var danmakuOptions: DanmakuOptions { get set }
   var isVisible: Bool { get set }
